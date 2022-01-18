fb76a78a835c7f485a075b02227e66a85f9e8818
Lucas Lins <Lucas.Lins@senior.com.br>
2022-01-18 17:28:49 -0300

    creating models/components/guards/services/features

---
+++ src/app/core/components/page-not-found/page-not-found.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-components',
+  templateUrl: './page-not-found.component.html',
+  styleUrls: ['./page-not-found.component.css']
+})
+export class ComponentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
